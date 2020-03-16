8e06df94089be1aa0c1e0ef9b2ea7d72445c27c3
Caio <42775945+caiobsilva@users.noreply.github.com>
2020-03-16 09:54:47 -0300

    organização de layout

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   get "/articles", to: "articles#index"
 
-  get 'welcome/index'
-
   resources :articles
 
-  root 'welcome#index'
+  root 'articles#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
