5a266cff98043940d9b4fbfa2536fa914666963f
Caio <42775945+caiobsilva@users.noreply.github.com>
2020-03-17 09:32:51 -0300

    Fix da migração de db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_131057) do
+ActiveRecord::Schema.define(version: 2020_03_17_122546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 2020_03_13_131057) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.datetime "deleted_at", precision: 6, null: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "deleted_at"
   end
 
 end
